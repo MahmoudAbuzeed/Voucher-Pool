167e0e9b95423e54659e1b32d55d19a1d8c74add
Mahmoud Abuzeed <m.yazed@awaed.co>
2023-08-05 12:03:28 +0300

    update the voucher code

--- src/voucher code/voucher-code.service.ts
+++ src/voucher code/voucher-code.service.ts
@@ -1,15 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { CREATED_SUCCESSFULLY, DELETED_SUCCESSFULLY, UPDATED_SUCCESSFULLY } from "messages";
 import { Injectable } from "@nestjs/common";
 
-import { CreateVoucherCodeDto } from "./dto/create-voucher-code.dto";
-import { UpdateVoucherCodeDto } from "./dto/update-voucher-code.dto";
-import { ErrorHandler } from "shared/errorHandler.service";
-import { VoucherCodeRepo } from "./voucher-code.repository";
-import { ulid } from "ulid";
-import { ValidateVoucherCodeDto } from "./dto/validate-voucher-code.dto";
-import { CustomerService } from "src/customer/customer.service";
+import { SpecialOffer } from "src/special offer/entities/special-offer.entity";
 import { SpecialOfferService } from "src/Special Offer/special-offer.service";
+import { ValidateVoucherCodeDto } from "./dto/validate-voucher-code.dto";
+import { UpdateVoucherCodeDto } from "./dto/update-voucher-code.dto";
+import { CreateVoucherCodeDto } from "./dto/create-voucher-code.dto";
+import { Customer } from "src/customer/entities/customer.entity";
+import { CustomerService } from "src/customer/customer.service";
 import { VoucherCode } from "./entities/voucher-code.entity";
+import { VoucherCodeRepo } from "./voucher-code.repository";
+import { ErrorHandler } from "shared/errorHandler.service";
+const voucher_codes = require("voucher-code-generator");
 
 @Injectable()
 export class VoucherCodeService {
@@ -21,20 +24,46 @@ export class VoucherCodeService {
   ) {}
 
   async create(createVoucherCodeDto: CreateVoucherCodeDto) {
+    const { customer, specialOffer } = await this.findCustomerAndOffer(createVoucherCodeDto);
+    const voucherCode = this.constructVoucherCode(customer, specialOffer);
+    await this.voucherCodeRepo.create(voucherCode);
+    return { message: CREATED_SUCCESSFULLY };
+  }
+
+  private async findCustomerAndOffer(createVoucherCodeDto: CreateVoucherCodeDto) {
     const customer = await this.customerService.findOneByEmail(createVoucherCodeDto.customer_email);
-
     const specialOffer = await this.specialOfferService.findOne(createVoucherCodeDto.special_offer_id);
+    return { customer, specialOffer };
+  }
 
+  private constructVoucherCode(customer: Customer, specialOffer: SpecialOffer): VoucherCode {
     const voucherCode = new VoucherCode();
-    voucherCode.code = ulid();
+    const code = voucher_codes.generate({ length: 8 });
+
+    voucherCode.code = code[0].toUpperCase();
     voucherCode.expired_at = new Date();
     voucherCode.expired_at.setDate(voucherCode.expired_at.getDate() + 3);
     voucherCode.used_at = null;
     voucherCode.customer = customer;
     voucherCode.specialOffer = specialOffer;
+    return voucherCode;
+  }
 
-    await this.voucherCodeRepo.create(voucherCode);
-    return { message: CREATED_SUCCESSFULLY };
+  async validateVoucher(validateVoucherCodeDto: ValidateVoucherCodeDto) {
+    const voucherCode = await this.voucherCodeRepo.findByCode(validateVoucherCodeDto.code);
+    if (!voucherCode) throw this.errorHandler.notFound();
+    if (voucherCode.customer.email != validateVoucherCodeDto.customer_email) {
+      throw this.errorHandler.invalidVoucherCode();
+    }
+
+    if (voucherCode.expired_at < new Date()) throw this.errorHandler.expiredVoucherCode();
+
+    await this.voucherCodeRepo.update(voucherCode.id, {
+      used: true,
+      used_at: new Date(),
+    });
+
+    return voucherCode;
   }
 
   async findAll() {
@@ -67,22 +96,5 @@ export class VoucherCodeService {
 
   async getOneByMail(email: string) {
     return await this.voucherCodeRepo.findByMail(email);
-  }
-
-  async validateVoucher(validateVoucherCodeDto: ValidateVoucherCodeDto) {
-    const voucherCode = await this.voucherCodeRepo.findByCode(validateVoucherCodeDto.code);
-    if (!voucherCode) throw this.errorHandler.notFound();
-    if (voucherCode.customer.email != validateVoucherCodeDto.customer_email) {
-      throw this.errorHandler.invalidVoucherCode();
-    }
-
-    if (voucherCode.expired_at < new Date()) throw this.errorHandler.expiredVoucherCode();
-
-    await this.voucherCodeRepo.update(voucherCode.id, {
-      used: true,
-      used_at: new Date(),
-    });
-
-    return voucherCode;
   }
 }
