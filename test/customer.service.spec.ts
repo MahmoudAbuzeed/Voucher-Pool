3b008aea619c034a40ce5b31a8bfd246236700a4
Mahmoud Abuzeed <m.yazed@awaed.co>
2023-08-05 15:16:52 +0300

    write create customer test cases

--- test/customer.service.spec.ts
+++ test/customer.service.spec.ts
@@ -2,9 +2,15 @@ import { Test, TestingModule } from "@nestjs/testing";
 
 import { CustomerRepository } from "../src/customer/customer.repository";
 import { CustomerService } from "../src/customer/customer.service";
+import { CreateCustomerDto } from "../src/customer/dto/create-customer.dto";
+import { CustomError } from "../shared/custom-error/custom-error";
 
-export const mockedCustomerRepository = () => ({
-  saveApplication: jest.fn(),
+const createMock = jest.fn();
+const findByMailMock = jest.fn();
+
+const mockedCustomerRepository = () => ({
+  create: createMock,
+  findByMail: findByMailMock,
 });
 
 describe("CustomerService", () => {
@@ -19,9 +25,52 @@ describe("CustomerService", () => {
     customerService = module.get<CustomerService>(CustomerService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe("My Test Suite", () => {
     it("should pass", () => {
       expect(true).toBe(true);
+    });
+  });
+
+  describe("create", () => {
+    const createCustomerDto: CreateCustomerDto = {
+      email: "example@example.com",
+      name: "John Doe",
+    };
+
+    it("should create a customer successfully", async () => {
+      createMock.mockResolvedValue(createCustomerDto);
+      findByMailMock.mockResolvedValue(null);
+
+      const result = await customerService.create(createCustomerDto);
+
+      expect(result.message).toBe("CREATED_SUCCESSFULLY");
+    });
+
+    it("should throw an error if the customer already exists", async () => {
+      findByMailMock.mockResolvedValue(createCustomerDto);
+
+      await expect(customerService.create(createCustomerDto)).rejects.toThrow(
+        new CustomError(400, "Customer already exists!"),
+      );
+    });
+
+    it("should throw an error if the email is invalid", async () => {
+      const invalidDto = { ...createCustomerDto, email: "invalidemail" };
+
+      await expect(customerService.create(invalidDto)).rejects.toThrow(new CustomError(400, "Invalid email address!"));
+    });
+
+    it("should throw an error if something goes wrong during creation", async () => {
+      createMock.mockResolvedValue(null);
+      findByMailMock.mockResolvedValue(null);
+
+      await expect(customerService.create(createCustomerDto)).rejects.toThrow(
+        new CustomError(400, "Something went wrong!"),
+      );
     });
   });
 });
